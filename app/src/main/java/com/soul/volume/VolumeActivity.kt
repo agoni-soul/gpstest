64c71b9ec436c384356181e1da8b20f4cf2b701f
haha <yangzy_agoni@163.com>
2024-05-14 18:12:07 +0800

    feat:增加对媒体音播放的逻辑,部分完成

--- app/src/main/java/com/soul/volume/VolumeActivity.kt
+++ app/src/main/java/com/soul/volume/VolumeActivity.kt
@@ -4,15 +4,15 @@ import android.content.Context
 import android.content.IntentFilter
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.soul.base.BaseActivity
 import com.soul.base.BaseMvvmActivity
-import com.soul.base.BaseViewModel
 import com.soul.gpstest.R
 import com.soul.gpstest.databinding.ActivityVolumeBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 
 /**
@@ -21,18 +21,23 @@ import com.soul.gpstest.databinding.ActivityVolumeBinding
  *     desc   :
  *     version: 1.0
  */
-class VolumeActivity : BaseMvvmActivity<ActivityVolumeBinding, BaseViewModel>() {
+class VolumeActivity : BaseMvvmActivity<ActivityVolumeBinding, VolumeViewModel>() {
 
-    private val mVolumeRecyclerView: RecyclerView by lazy {
-        findViewById(R.id.rv_volume)
+    // 调节音量
+    private lateinit var mAudioManager: AudioManager
+    private lateinit var mVolumeBroadReceiver: VolumeBroadReceiver
+    // 媒体播放器
+    private lateinit var mMediaPlayer: MediaPlayer
+    private val mOnPreparedListener: MediaPlayer.OnPreparedListener by lazy {
+        object : MediaPlayer.OnPreparedListener {
+            override fun onPrepared(mp: MediaPlayer?) {
+            }
+        }
     }
 
-    private lateinit var mAudioManager: AudioManager
-    private lateinit var mMediaPlayer: MediaPlayer
-    private lateinit var mVolumeBroadReceiver: VolumeBroadReceiver
     private lateinit var mVolumeAdjustAdapter: VolumeAdjustAdapter
 
-    override fun getViewModelClass(): Class<BaseViewModel> = BaseViewModel::class.java
+    override fun getViewModelClass(): Class<VolumeViewModel> = VolumeViewModel::class.java
 
     override fun getLayoutId(): Int = R.layout.activity_volume
 
@@ -69,18 +74,40 @@ class VolumeActivity : BaseMvvmActivity<ActivityVolumeBinding, BaseViewModel>() 
             )
             mVolumeAdjustAdapter.updateVolumeData(volumeList)
             mVolumeAdjustAdapter.setHasStableIds(true)
-            mVolumeRecyclerView.adapter = mVolumeAdjustAdapter
+            rvVolume.adapter = mVolumeAdjustAdapter
+
+            tvPlayer.setOnClickListener {
+                Log.d(TAG, "isPlaying = ${mMediaPlayer.isPlaying}")
+                if (!mMediaPlayer.isPlaying) {
+                    mMediaPlayer.start()
+                }
+            }
         }
     }
 
     override fun initData() {
         mAudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
         mMediaPlayer = MediaPlayer()
+        mMediaPlayer.setOnPreparedListener(mOnPreparedListener)
+        initMusic()
+    }
 
+    private fun initMusic() {
+        MainScope().launch {
+            try {
+                val uri = "http://isure6.stream.qqmusic.qq.com/C200001KBxQw0PWq7Y.m4a?guid=2000000194&vkey=8F7A8529D0AA51DE9742C450949E9528AD5B7D98F851657AA06189E992067E2A28F51C0CC6B42C577C61264804DA728EC4DE743DFD583E9D&uin=0&fromtag=20194&trace=5cdf245b6863abb0"
+                mMediaPlayer.setDataSource(mContext, Uri.parse(uri))
+                mMediaPlayer.prepare()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(mVolumeBroadReceiver)
+        mMediaPlayer.stop()
+        mMediaPlayer.release()
     }
 }

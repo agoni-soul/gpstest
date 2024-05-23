cf148fdf836adee5bd4699e7edb10b1933deb636
haha <yangzy_agoni@163.com>
2024-05-23 13:45:02 +0800

    feat:优化组件

---
+++ app/src/main/java/com/soul/easyswipemenulayout/EasySwipeMenuActivity.kt
@@ -0,0 +1,10 @@
+package com.soul.easyswipemenulayout
+
+
+/**
+ *     author : yangzy33
+ *     time   : 2024-05-23
+ *     desc   : 
+ *     version: 1.0
+ */class EasySwipeMenuActivity {
+}
---
+++ app/src/main/java/com/soul/easyswipemenulayout/EasySwipeMenuLayout.java
@@ -0,0 +1,8 @@
+package com.soul.easyswipemenulayout;
+/**
+ *     author : yangzy33
+ *     time   : 2024-05-23
+ *     desc   : 
+ *     version: 1.0
+ */public class EasySwipeMenuLayout {
+}

dcc9613d76822f1ca0d52c3266f8f95f9d405954
haha <yangzy_agoni@163.com>
2024-06-14 17:49:05 +0800

    feat:完成播放音乐的进度的初稿

--- app/src/main/java/com/soul/volume/VolumeActivity.kt
+++ app/src/main/java/com/soul/volume/VolumeActivity.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.content.IntentFilter
 import android.media.AudioManager
 import android.media.MediaPlayer
+import android.media.MediaPlayer.OnBufferingUpdateListener
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.soul.base.BaseMvvmActivity
@@ -76,28 +81,44 @@ class VolumeActivity : BaseMvvmActivity<ActivityVolumeBinding, VolumeViewModel>(
                 Log.d(TAG, "isMediaPrepare = ${mViewModel?.getIsMediaPrepare()?.value}, isPlaying = ${mMediaPlayer.isPlaying}")
                 if (mViewModel?.getIsMediaPrepare()?.value == true && !mMediaPlayer.isPlaying) {
                     mMediaPlayer.start()
+                    Log.d(TAG, "setOnClickListener: duration = ${mMediaPlayer.duration}")
                     it.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_play, null)
                 } else if (mMediaPlayer.isPlaying) {
                     mMediaPlayer.pause()
                     it.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_pause, null)
+                } else {
+                    it.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_pause_unable, null)
                 }
             }
+
+            sbMusicProgress.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        mMediaPlayer.seekTo(progress)
+                        Log.d(TAG, "onProgressChanged: progress = $progress")
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                }
+
+            })
         }
     }
 
     override fun initData() {
-        mAudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        mMediaPlayer = MediaPlayer.create(this, R.raw.raw_music)
-        mMediaPlayer.setOnPreparedListener {
-            mViewModel?.getIsMediaPrepare()?.postValue(true)
-        }
-        mMediaPlayer.setOnErrorListener(object : MediaPlayer.OnErrorListener {
-            override fun onError(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
-                Log.e(TAG, "MediaPlayer onError")
-                return true
-            }
-        })
         initMusic()
+        mViewDataBinding?.sbMusicProgress?.max = mMediaPlayer.duration
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mViewDataBinding?.sbMusicProgress?.min = 0
+        }
 
         mViewModel?.apply {
             getIsMediaPrepare().observe(this@VolumeActivity) {
@@ -111,13 +132,33 @@ class VolumeActivity : BaseMvvmActivity<ActivityVolumeBinding, VolumeViewModel>(
 
     private fun initMusic() {
         try {
+            mAudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+            mMediaPlayer = MediaPlayer.create(this, R.raw.raw_music)
+            mMediaPlayer.setOnPreparedListener {
+                mViewModel?.getIsMediaPrepare()?.postValue(true)
+            }
+            mMediaPlayer.setOnCompletionListener {
+                Log.d(TAG, "initMusic: duration = ${mMediaPlayer.duration}")
+                Toast.makeText(mContext, "播放完成", Toast.LENGTH_SHORT).show()
+                mViewDataBinding?.ivVolumePlay?.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_pause, null)
+            }
+            mMediaPlayer.setOnBufferingUpdateListener(object : OnBufferingUpdateListener {
+                override fun onBufferingUpdate(mp: MediaPlayer?, percent: Int) {
+                    Log.d(TAG, "onBufferingUpdate: percent = $percent")
+                }
+            })
+            mMediaPlayer.setOnErrorListener(object : MediaPlayer.OnErrorListener {
+                override fun onError(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
+                    Log.e(TAG, "MediaPlayer onError")
+                    mp?.release()
+                    return true
+                }
+            })
             DOFLogUtil.d(TAG, "1")
 //            mMediaPlayer.reset()
             DOFLogUtil.d(TAG, "2")
-            mMediaPlayer = MediaPlayer.create(this, R.raw.raw_music)
-            DOFLogUtil.d(TAG, "3")
             mMediaPlayer.prepare()
-            DOFLogUtil.d(TAG, "4")
+            DOFLogUtil.d(TAG, "3")
         } catch (e: Exception) {
             e.printStackTrace()
             Log.e(TAG, "initMusic: exception = ${e.message}")

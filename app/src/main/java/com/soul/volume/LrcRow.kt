db7bbe051b67cf14ed2ef74e66023d8ff5e35133
haha <yangzy_agoni@163.com>
2024-06-21 18:13:31 +0800

    feat:增加歌曲歌词逻辑, 部分完成

---
+++ app/src/main/java/com/soul/volume/LrcRow.kt
@@ -0,0 +1,68 @@
+package com.soul.volume
+
+
+/**
+ *     author : yangzy33
+ *     time   : 2024-06-21
+ *     desc   :
+ *     version: 1.0
+ */
+class LrcRow() : Comparable<LrcRow> {
+    private val TAG = javaClass.simpleName
+
+    private var strTime: String? = null
+    private var time: Long = 0
+    private var content: String? = null
+
+    constructor(strTime: String, time: Long, content: String) : this() {
+        this.strTime = strTime
+        this.time = time
+        this.content = content
+    }
+
+    override fun toString(): String {
+        return "[$strTime] $content"
+    }
+
+    override fun compareTo(other: LrcRow): Int {
+
+    }
+
+    companion object {
+        fun createRows(standardLrcLine: String): MutableList<LrcRow>? {
+            try {
+                if (standardLrcLine.indexOf("[") != 0) {
+                    return null
+                }
+                val lastIndexOfRightBracket = standardLrcLine.lastIndexOf("]")
+                val content = standardLrcLine.substring(lastIndexOfRightBracket + 1)
+                val times =
+                    standardLrcLine.substring(0, lastIndexOfRightBracket + 1)
+                        .replace("[", "-")
+                        .replace("]", "-")
+                val arrTimes = times.split("-")
+                val listTimes = mutableListOf<LrcRow>()
+                for (temp in arrTimes) {
+                    if (temp.trim().isEmpty()) {
+                        continue
+                    }
+                    val lrcRow = LrcRow(temp, timeConvert(temp), content)
+                    listTimes.add(lrcRow)
+                }
+                return null
+            } catch (e: Exception) {
+                e.printStackTrace()
+                return null
+            }
+        }
+
+        private fun timeConvert(timeString: String): Long {
+            val timeTemp = timeString.replace(".", ":")
+            val times = timeTemp.split(":")
+            val minute = times[0].toIntOrNull() ?: 0
+            val second = times[1].toIntOrNull() ?: 0
+            var milli = times[2].toIntOrNull() ?: 0
+            return minute * 60 * 1000L + second * 1000L + milli
+        }
+    }
+}

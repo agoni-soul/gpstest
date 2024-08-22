cb43f778dfa95c4c114dc40778a8b2d0018e4760
haha <yangzy_agoni@163.com>
2024-08-22 18:02:18 +0800

    refactor:蓝牙Gatt服务端逻辑重构

--- app/src/main/java/com/soul/bluetooth/BleServerFragment.kt
+++ app/src/main/java/com/soul/bluetooth/BleServerFragment.kt
@@ -3,16 +3,13 @@ package com.soul.bluetooth
 import android.Manifest
 import android.bluetooth.*
 import android.bluetooth.le.AdvertiseCallback
-import android.bluetooth.le.AdvertiseData
 import android.bluetooth.le.AdvertiseSettings
-import android.content.pm.PackageManager
 import android.os.Build
-import android.os.ParcelUuid
 import android.util.Log
-import androidx.core.app.ActivityCompat
 import androidx.lifecycle.viewModelScope
 import com.soul.base.BaseMvvmFragment
 import com.soul.base.BaseViewModel
+import com.soul.bleSDK.commication.BleServerManager
 import com.soul.bleSDK.constants.BleBlueImpl
 import com.soul.bleSDK.manager.BleScanManager
 import com.soul.gpstest.R
@@ -212,117 +209,34 @@ class BleServerFragment : BaseMvvmFragment<FragmentBleServerBinding, BaseViewMod
     }
 
     private fun initBle() {
-        if (!PermissionUtils.checkMultiPermission(Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_ADVERTISE)) return
+        if (!PermissionUtils.checkMultiPermission(
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.BLUETOOTH_ADVERTISE
+            )
+        ) return
         bluetoothAdapter = BleScanManager.getBluetoothAdapter()
         bluetoothAdapter?.name = "k20"
-
-        /**
-         * GAP广播数据最长只能31个字节，包含两中： 广播数据和扫描回复
-         * - 广播数据是必须的，外设需要不断发送广播，让中心设备知道
-         * - 扫描回复是可选的，当中心设备扫描到才会扫描回复
-         * 广播间隔越长，越省电
-         */
-
-        //广播设置
-        val advSetting = AdvertiseSettings.Builder()
-            //低延时，高功率，不使用后台
-            .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-            // 高的发送功率
-            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-            // 可连接
-            .setConnectable(true)
-            //广播时限。最多180000毫秒。值为0将禁用时间限制。（不设置则为无限广播时长）
-            .setTimeout(0)
-            .build()
-        //设置广播包，这个是必须要设置的
-        val advData = AdvertiseData.Builder()
-            .setIncludeDeviceName(true) //显示名字
-            .setIncludeTxPowerLevel(true)//设置功率
-            .addServiceUuid(ParcelUuid(BleBlueImpl.UUID_SERVICE)) //设置 UUID 服务的 uuid
-            .build()
-
-
-        //测试 31bit
-        val byteData = byteArrayOf(
-            -65, 2, 3, 6, 4, 23, 23, 9, 9,
-            9, 1, 2, 3, 6, 4, 23, 23, 9, 9, 8, 23, 23, 23
-        )
-
-        //扫描广播数据（可不写，客户端扫描才发送）
-        val scanResponse = AdvertiseData.Builder()
-            //设置厂商数据
-            .addManufacturerData(0x19, byteData)
-            .build()
-
-
-        /**
-         * GATT 使用了 ATT 协议，ATT 把 service 和 characteristic 对应的数据保存在一个查询表中，
-         * 依次查找每一项的索引
-         * BLE 设备通过 Service 和 Characteristic 进行通信
-         * 外设只能被一个中心设备连接，一旦连接，就会停止广播，断开又会重新发送
-         * 但中心设备同时可以和多个外设连接
-         * 他们之间需要双向通信的话，唯一的方式就是建立 GATT 连接
-         * 外设作为 GATT(server)，它维持了 ATT 的查找表以及service 和 charateristic 的定义
-         */
-        val bluetoothLeAdvertiser = bluetoothAdapter?.bluetoothLeAdvertiser
-        //开启广播,这个外设就开始发送广播了
-        bluetoothLeAdvertiser?.startAdvertising(
-            advSetting,
-            advData,
-            scanResponse,
-            advertiseCallback
-        )
-
-        /**
-         * characteristic 是最小的逻辑单元
-         * 一个 characteristic 包含一个单一 value 变量 和 0-n个用来描述 characteristic 变量的
-         * Descriptor。与 service 相似，每个 characteristic 用 16bit或者32bit的uuid作为标识
-         * 实际的通信中，也是通过 Characteristic 进行读写通信的
-         */
-        //添加读+通知的 GattCharacteristic
-        val readCharacteristic = BluetoothGattCharacteristic(
-            BleBlueImpl.UUID_READ_NOTIFY,
-            BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_NOTIFY,
-            BluetoothGattCharacteristic.PERMISSION_READ
-        )
-
-        //添加写的 GattCharacteristic
-        val writeCharacteristic = BluetoothGattCharacteristic(
-            BleBlueImpl.UUID_WRITE,
-            BluetoothGattCharacteristic.PROPERTY_WRITE,
-            BluetoothGattCharacteristic.PERMISSION_WRITE
-        )
-
-        //添加 Descriptor 描述符
-        val descriptor =
-            BluetoothGattDescriptor(
-                BleBlueImpl.UUID_DESCRIBE,
-                BluetoothGattDescriptor.PERMISSION_WRITE
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            BleServerManager.launchAdvertising(bluetoothAdapter)
+        }
+        BleServerManager.getBleGattService(
+            BleBlueImpl.UUID_DESCRIBE,
+            BluetoothGattDescriptor.PERMISSION_WRITE
+        ).apply {
+            BleServerManager.createReadGattCharacteristic(this, BleBlueImpl.UUID_READ_NOTIFY)
+            BleServerManager.createWriteGattCharacteristic(
+                this,
+                BleBlueImpl.UUID_WRITE,
+                BleBlueImpl.UUID_DESCRIBE
             )
-
-        //为特征值添加描述
-        writeCharacteristic.addDescriptor(descriptor)
-
-
-        /**
-         * 添加 Gatt service 用来通信
-         */
-
-        //开启广播service，这样才能通信，包含一个或多个 characteristic ，每个service 都有一个 uuid
-        val gattService =
-            BluetoothGattService(
-                BleBlueImpl.UUID_SERVICE,
-                BluetoothGattService.SERVICE_TYPE_PRIMARY
-            )
-        gattService.addCharacteristic(readCharacteristic)
-        gattService.addCharacteristic(writeCharacteristic)
-
-
-        val bluetoothManager = BleScanManager.getBluetoothManager()
-        //打开 GATT 服务，方便客户端连接
-        mBluetoothGattServer =
-            bluetoothManager?.openGattServer(requireContext(), gattServiceCallback)
-        mBluetoothGattServer?.addService(gattService)
+            val bluetoothManager = BleScanManager.getBluetoothManager()
+            //打开 GATT 服务，方便客户端连接
+            mBluetoothGattServer =
+                bluetoothManager?.openGattServer(requireContext(), gattServiceCallback)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                BleServerManager.addService(mBluetoothGattServer, this)
+            }
+        }
     }
 
     private fun logInfo(msg: String) {
@@ -335,7 +249,9 @@ class BleServerFragment : BaseMvvmFragment<FragmentBleServerBinding, BaseViewMod
     @SuppressWarnings("missingPermission")
     override fun onDestroy() {
         super.onDestroy()
-        bluetoothAdapter?.bluetoothLeAdvertiser?.stopAdvertising(advertiseCallback)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            BleServerManager.stopAdvertising(bluetoothAdapter)
+        }
         mBluetoothGattServer?.close()
     }
 }

04fb310fcb3f8dd3eab61f26439cd4d40717181e
haha <yangzy_agoni@163.com>
2024-09-05 15:21:44 +0800

    feat:完成针对AutoService在java中功能的实现

---
+++ compiler/src/main/java/com/haha/processor/MConstants.java
@@ -0,0 +1,16 @@
+//package com.haha.processor;
+//
+//import com.squareup.javapoet.ClassName;
+//
+//public class MConstants {
+//
+//
+//    public static ClassName CLASSNAME_LOG       = ClassName.get("android.util", "Log");
+//
+//    public static ClassName CLASSNAME_UI_THREAD = ClassName.get("android.support.annotation", "UiThread");
+//
+//    public static ClassName CLASSNAME_VIEW      = ClassName.get("android.view", "View");
+//
+//    public static String    INJECT_NAME         ="inject";
+//
+//}

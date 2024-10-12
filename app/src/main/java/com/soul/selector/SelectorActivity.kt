72a2bf02a91d30fca79f239bafd3863af356c41c
haha <yangzy_agoni@163.com>
2024-10-12 18:36:37 +0800

    feat:优化SelectorActivity的逻辑

--- app/src/main/java/com/soul/selector/SelectorActivity.kt
+++ app/src/main/java/com/soul/selector/SelectorActivity.kt
@@ -1,6 +1,7 @@
 package com.soul.selector
 
-import android.widget.TextView
+import android.util.Log
+import android.view.MotionEvent
 import com.soul.base.BaseMvvmActivity
 import com.soul.base.BaseViewModel
 import com.soul.gpstest.R
@@ -14,69 +15,73 @@ import com.soul.gpstest.databinding.ActivitySelectorBinding
  *     version: 1.0
  */
 class SelectorActivity: BaseMvvmActivity<ActivitySelectorBinding, BaseViewModel>() {
-    private val mTvDS: TextView by lazy {
-        findViewById(R.id.tv_ds)
-    }
-    private val mTvDsEnable: TextView by lazy {
-        findViewById(R.id.tv_ds_enable)
-    }
-    private val mTvDsPress: TextView by lazy {
-        findViewById(R.id.tv_ds_press)
-    }
-    private val mTvCount: TextView by lazy {
-        findViewById(R.id.tv_click_count)
-    }
     private var mCount = 0
+
     override fun getViewModelClass(): Class<BaseViewModel> = BaseViewModel::class.java
 
     override fun getLayoutId(): Int = R.layout.activity_selector
 
     override fun initView() {
-        mTvDS.setOnClickListener {
-            mCount ++
-            mTvCount.text = mCount.toString()
-            when (mCount % 6) {
-                0 -> {
-                    mTvDsEnable.isEnabled = true
+        mViewDataBinding.apply {
+            tvDs.setOnClickListener {
+                mCount ++
+                tvClickCount.text = mCount.toString()
+                when (mCount % 6) {
+                    0 -> {
+                        tvDsEnable.isEnabled = true
 
-                    mTvDsPress.isEnabled = true
-                    mTvDsPress.isClickable = true
-                    mTvDsPress.isFocusable = true
-                    mTvDsPress.requestFocus()
-                }
-                1 -> {
-                    mTvDsEnable.isEnabled = false
+                        tvDsPress.isEnabled = true
+                        tvDsPress.isClickable = true
+                        tvDsPress.isFocusable = true
+                        tvDsPress.requestFocus()
+                    }
+                    1 -> {
+                        tvDsEnable.isEnabled = false
 
-                    mTvDsPress.isEnabled = true
-                    mTvDsPress.isClickable = false
-                    mTvDsPress.isFocusable = true
-                    mTvDsPress.requestFocus()
-                }
-                2 -> {
-                    mTvDsPress.isEnabled = true
-                    mTvDsPress.isClickable = true
-                    mTvDsPress.isFocusable = false
-                    mTvDsPress.requestFocus()
-                }
-                3 -> {
-                    mTvDsPress.isEnabled = false
-                    mTvDsPress.isClickable = true
-                    mTvDsPress.isFocusable = false
-                    mTvDsPress.requestFocus()
-                }
-                4 -> {
-                    mTvDsPress.isEnabled = true
-                    mTvDsPress.isClickable = false
-                    mTvDsPress.isFocusable = false
-                    mTvDsPress.requestFocus()
-                }
-                5 -> {
-                    mTvDsPress.isEnabled = false
-                    mTvDsPress.isClickable = false
-                    mTvDsPress.isFocusable = true
-                    mTvDsPress.requestFocus()
+                        tvDsPress.isEnabled = true
+                        tvDsPress.isClickable = false
+                        tvDsPress.isFocusable = true
+                        tvDsPress.requestFocus()
+                    }
+                    2 -> {
+                        tvDsPress.isEnabled = true
+                        tvDsPress.isClickable = true
+                        tvDsPress.isFocusable = false
+                        tvDsPress.requestFocus()
+                    }
+                    3 -> {
+                        tvDsPress.isEnabled = false
+                        tvDsPress.isClickable = true
+                        tvDsPress.isFocusable = false
+                        tvDsPress.requestFocus()
+                    }
+                    4 -> {
+                        tvDsPress.isEnabled = true
+                        tvDsPress.isClickable = false
+                        tvDsPress.isFocusable = false
+                        tvDsPress.requestFocus()
+                    }
+                    5 -> {
+                        tvDsPress.isEnabled = false
+                        tvDsPress.isClickable = false
+                        tvDsPress.isFocusable = true
+                        tvDsPress.requestFocus()
+                    }
                 }
             }
+
+            cbSelect.setDefaultAllDrawables()
+            cbSelect.setOnClickListener {
+                cbSelect.isChecked = !cbSelect.isChecked
+                if (cbSelect.isChecked) {
+                    cbSelect.background = getDrawable(R.drawable.drawable_shape_checkbox_select)
+                } else {
+                    cbSelect.background = getDrawable(R.drawable.drawable_shape_checkbox_null)
+                }
+                Log.d(TAG, "setOnClickListener: isChecked = ${cbSelect.isChecked}")
+            }
+
+
         }
     }
 

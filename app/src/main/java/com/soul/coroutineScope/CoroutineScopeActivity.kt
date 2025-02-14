b990341cabce4513a15bd9f7d34957e419666dc0
soul <yangzy_agoni@163.com>
2025-02-14 14:36:33 +0800

    feat:协程逻辑梳理

--- app/src/main/java/com/soul/coroutineScope/CoroutineScopeActivity.kt
+++ app/src/main/java/com/soul/coroutineScope/CoroutineScopeActivity.kt
@@ -18,7 +18,7 @@ import kotlinx.coroutines.*
  *     version: 1.0
  * </pre>
  */
-class CoroutineScopeActivity: BaseMvvmActivity<ActivityCoroutineScopeBinding, BaseViewModel>() {
+class CoroutineScopeActivity: BaseMvvmActivity<ActivityCoroutineScopeBinding, CoroutineScopeViewModel>() {
 
     private val mBtnStart: Button by lazy {
         findViewById(R.id.btn_start_coroutine_scope)
@@ -36,126 +36,25 @@ class CoroutineScopeActivity: BaseMvvmActivity<ActivityCoroutineScopeBinding, Ba
         findViewById(R.id.tv_coroutine_scope_second)
     }
 
-    override fun getViewModelClass(): Class<BaseViewModel> = BaseViewModel::class.java
+    override fun getViewModelClass(): Class<CoroutineScopeViewModel> = CoroutineScopeViewModel::class.java
 
     override fun getLayoutId(): Int = R.layout.activity_coroutine_scope
 
     override fun initView() {
         mBtnStart.setOnClickListener {
-            val array = ArrayList<String>()
-            array.add("abce")
-            array.add("abeef")
-            array.add("abcefd")
-            array.add("abcefd")
-            array.add("abafdce")
-            array.add("abcfdae")
-            array.add("abcefad")
-            array.add("abdefdce")
-            array.add("addbce")
-            array.add("abce")
-            array.add("adfdbce")
-            array.add("addbce")
-            array.add("addbce")
-            array.add("adbce")
-            array.add("abcDEd")
-            array.add("abcfdfdde")
-            array.add("abcDed")
-            array.add("abcDED")
-            array.add("abcDEd")
-            array.add("abcefd")
-            array.add("abcefd")
-            array.add("abafdce")
-            array.add("abcfdae")
-            array.add("abcefad")
-            array.add("abdefdce")
-            for (s in array) {
-                filterDevice(s)
-            }
+            mViewModel.startScan()
         }
         mBtnCancel.setOnClickListener {
-            mScanDeviceJob?.cancelChildren()
+            mViewModel.stopScan()
+        }
+        mViewModel.mSsidFirstData.observe(this) {
+            mTvFirstScope.text = it
+        }
+        mViewModel.mSsidSecondData.observe(this) {
+            mTvSecondScope.text = it
         }
     }
 
     override fun initData() {
-    }
-
-    private var mSSID = "abcd"
-    @Volatile
-    private var mIsPreciseMatch: Boolean = false
-    private var mScanDeviceJob : Job? = null
-    private var mFindDeviceJob : Job? = null
-
-    private fun filterDevice(s: String) {
-        mScanDeviceJob = CoroutineScope(Dispatchers.Default).launch {
-            if (mIsPreciseMatch) {
-                runBlocking {
-                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, ${Thread.currentThread()} hahahahh")
-                    if (mIsPreciseMatch) {
-                        if (mSSID == s) {
-                            GlobalScope.launch(Dispatchers.Main) {
-                                if (mIsPreciseMatch) {
-                                    mTvSecondScope.text = s
-                                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, ${Thread.currentThread()} hahahahh")
-                                }
-                            }
-                        }
-                    }
-                }
-            } else {
-                runBlocking {
-                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, ${Thread.currentThread()} mFindDeviceJob")
-                    if (!mIsPreciseMatch) {
-                        if (s.contains(mSSID, true)) {
-                            GlobalScope.launch(Dispatchers.Main) {
-                                if (!mIsPreciseMatch) {
-                                    mIsPreciseMatch = true
-                                    mSSID = s
-                                    mTvFirstScope.text = s
-                                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.Main mFindDeviceJob")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-//            synchronized(mIsPreciseMatch) {
-//                Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.Default hahahahh")
-//                if (mIsPreciseMatch) {
-//                    if (mSSID == s) {
-//                        GlobalScope.launch(Dispatchers.Main) {
-//                            if (mIsPreciseMatch) {
-//                                mTvSecondScope.text = s
-//                                Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.Main hahahahh")
-//                            }
-//                        }
-//                    }
-//                } else {
-//                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.IO mFindDeviceJob")
-//                    if (s.contains(mSSID, true)) {
-//                        GlobalScope.launch(Dispatchers.Main) {
-//                            if (!mIsPreciseMatch) {
-//                                mIsPreciseMatch = true
-//                                mSSID = s
-//                                mTvFirstScope.text = s
-//                                Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.Main mFindDeviceJob")
-//                            } else {
-//                                Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.IO 异步线程")
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-            if (mIsPreciseMatch) {
-//                mFindDeviceJob?.let {
-//                    if (it.isActive) {
-//                        Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, mFindDeviceJob 取消")
-//                        it.cancelChildren()
-//                    }
-//                }
-            } else {
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/soul/coroutineScope/CoroutineScopeViewModel.kt
@@ -0,0 +1,154 @@
+package com.soul.coroutineScope
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.soul.base.BaseViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ *
+ * @author:     haha
+ * @date:       2025/2/11
+ * Description: 协程数据Model
+ *
+ **/
+class CoroutineScopeViewModel(application: Application): BaseViewModel(application) {
+
+    private var mSSID = "abcd"
+    @Volatile
+    private var mIsPreciseMatch: Boolean = false
+    private var mScanDeviceJob : Job? = null
+    private var mFindDeviceJob : Job? = null
+
+    private var scopeTest: CoroutineScopeTest? = null
+
+    var mSsidFirstData: MutableLiveData<String> = MutableLiveData()
+
+    var mSsidSecondData: MutableLiveData<String> = MutableLiveData()
+
+    fun startScan() {
+        val array = initScanData()
+        for (s in array) {
+            filterDevice(s)
+        }
+
+        scopeTest = CoroutineScopeTest()
+        scopeTest?.start()
+    }
+
+    private fun initScanData(): MutableList<String> {
+        val array = ArrayList<String>()
+        array.add("abce")
+        array.add("abeef")
+        array.add("abcefd")
+        array.add("abcefd")
+        array.add("abafdce")
+        array.add("abcfdae")
+        array.add("abcefad")
+        array.add("abdefdce")
+        array.add("addbce")
+        array.add("abce")
+        array.add("adfdbce")
+        array.add("addbce")
+        array.add("addbce")
+        array.add("adbce")
+        array.add("abcDEd")
+        array.add("abcfdfdde")
+        array.add("abcDed")
+        array.add("abcDED")
+        array.add("abcDEd")
+        array.add("abcefd")
+        array.add("abcefd")
+        array.add("abafdce")
+        array.add("abcfdae")
+        array.add("abcefad")
+        array.add("abdefdce")
+        return array
+    }
+
+    private fun filterDevice(s: String) {
+        mScanDeviceJob = CoroutineScope(Dispatchers.Default).launch {
+            if (mIsPreciseMatch) {
+                runBlocking {
+                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, ${Thread.currentThread()} hahahahh")
+                    if (mIsPreciseMatch) {
+                        if (mSSID == s) {
+                            GlobalScope.launch(Dispatchers.Main) {
+                                if (mIsPreciseMatch) {
+                                    mSsidFirstData.postValue(s)
+                                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, ${Thread.currentThread()} hahahahh")
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                runBlocking {
+                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, ${Thread.currentThread()} mFindDeviceJob")
+                    if (!mIsPreciseMatch) {
+                        if (s.contains(mSSID, true)) {
+                            GlobalScope.launch(Dispatchers.Main) {
+                                if (!mIsPreciseMatch) {
+                                    mIsPreciseMatch = true
+                                    mSSID = s
+                                    mSsidSecondData.postValue(s)
+                                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.Main mFindDeviceJob")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+//            synchronized(mIsPreciseMatch) {
+//                Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.Default hahahahh")
+//                if (mIsPreciseMatch) {
+//                    if (mSSID == s) {
+//                        GlobalScope.launch(Dispatchers.Main) {
+//                            if (mIsPreciseMatch) {
+//                                mTvSecondScope.text = s
+//                                Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.Main hahahahh")
+//                            }
+//                        }
+//                    }
+//                } else {
+//                    Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.IO mFindDeviceJob")
+//                    if (s.contains(mSSID, true)) {
+//                        GlobalScope.launch(Dispatchers.Main) {
+//                            if (!mIsPreciseMatch) {
+//                                mIsPreciseMatch = true
+//                                mSSID = s
+//                                mTvFirstScope.text = s
+//                                Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.Main mFindDeviceJob")
+//                            } else {
+//                                Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, Dispatchers.IO 异步线程")
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+            if (mIsPreciseMatch) {
+//                mFindDeviceJob?.let {
+//                    if (it.isActive) {
+//                        Log.d(TAG, "mIsPreciseMatch = $mIsPreciseMatch, mSSID = $mSSID, s = $s, mFindDeviceJob 取消")
+//                        it.cancelChildren()
+//                    }
+//                }
+            } else {
+            }
+        }
+    }
+
+    fun stopScan() {
+        mScanDeviceJob?.cancelChildren()
+        scopeTest?.stop()
+    }
+}

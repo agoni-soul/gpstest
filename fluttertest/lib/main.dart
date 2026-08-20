e2d4f088cb044dc7d73b4831db2d5796f5df6bc4
haha <57203442+agoni-soul@users.noreply.github.com>
2026-08-21 06:19:45 +0800

    feat:修复flutter运行出错的问题

--- fluttertest/lib/main.dart
+++ fluttertest/lib/main.dart
@@ -1,15 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertest/page/home/home_page.dart';
+import 'package:fluttertest/app.dart';
+import 'package:fluttertest/common/logger.dart';
+import 'package:fluttertest/env/config_wrapper.dart';
+import 'package:fluttertest/env/dev.dart';
+import 'package:fluttertest/env/env_config.dart';
 
 void main() {
-  runApp(DemoApp());
-}
+  runZonedGuarded(() {
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      Zone.current.handleUncaughtError(details.exception, details.stack!);
+      return Material(
+        color: Colors.white,
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Text(
+              '${details.exception}\n${details.stack}',
+              style: const TextStyle(color: Colors.red, fontSize: 12),
+            ),
+          ),
+        ),
+      );
+    };
+    runApp(ConfigWrapper(
+      config: EnvConfig.fromJson(config),
+      child: const FlutterReduxApp(),
+    ));
 
-class DemoApp extends StatelessWidget {
-  const DemoApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage(),);
-  }
+    /// 屏幕刷新率和显示率不一致时的优化，必须挪动到 runApp 之后
+    GestureBinding.instance.resamplingEnabled = true;
+  }, (Object obj, StackTrace stack) {
+    talker.error('Catch Dart error:', obj, stack);
+    printLog(obj);
+    printLog(stack);
+  });
 }
